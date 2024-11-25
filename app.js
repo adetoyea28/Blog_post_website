1b6b4d3245b4c69408a6211b9aaa728181dd312f
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2024-11-25 02:23:17 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// Imported all modules required
+
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Initializing the application
+const app = express();
+// setting the port
+const port = 4000;
+
+// Backend logic goes here
+// -----------------------
+
+
+
+// -----------------------
+
+// starts the server.
+app.listen(port, () => {
+    console.log("server running at http://localhost:4000");
+});
