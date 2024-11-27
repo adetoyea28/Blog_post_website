c6b11a81d2d79b03a9270023d935ac65ca4cd1f1
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2024-11-27 12:46:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
-// Imported all modules required
-
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-// Initializing the application
+
 const app = express();
-// setting the port
 const port = 4000;
 
 // Backend logic goes here
 // -----------------------
 
+const data = fs.readFileSync('db/db.json');
+const data_obj = JSON.parse(data);
 
+const no_of_blog = 3;
+let data_str_list = [];
+let i = 0;
 
+while(i < no_of_blog){
+    data_str_list.push("<div><h1>" + data_obj.example[i].title + "</h1><br>"+"<p>"+data_obj.example[i].body+"</p></div><br>");
+    i++;
+}
 
-// --------------------
+let data_str = data_str_list[0]+data_str_list[1]+data_str_list[2];
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res)=> {
+    res.setHeader('Content-Type', 'text/html');
+    res.render("index", { blog_post_website : data_str });
+})
+
 
 // starts the server.
 app.listen(port, () => {
